e100c1126e09cfe846bd86779e50664d387e5a76
AyanAlikhan11 <scoutayan853@gmail.com>
2026-01-31 10:27:01 +0530

    updated

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,8 +1,11 @@
 import AdminLoginPage from "@/src/components/Admin/Login/page";
+import { Suspense } from "react";
 
 const login = ()=>{
     return(
+       <Suspense fallback={<div>Loading...</div>}>
         <AdminLoginPage/>
+        </Suspense>
     )
 }
 
